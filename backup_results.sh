e8b47c6222c8a298c483080889499906f788c81d
Adam Bennett <43841526+a4000@users.noreply.github.com>
2026-07-28 11:25:12 +0800

    fix path

--- backup_results.sh
+++ backup_results.sh
@@ -5,5 +5,5 @@ project=$(basename "$(dirname "$project_dir")")
 rclone copy ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
 rclone check ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
 
-rclone copy ${project_dir}/*/*/01-cutadapt/assigned* s3:ocom-edna/illumina-meta-sra/${project}/
-rclone check ${project_dir}/*/*/01-cutadapt/assigned* s3:ocom-edna/illumina-meta-sra/${project}/
+rclone copy ${project_dir}/*/*/01-cutadapt/assigned/* s3:ocom-edna/illumina-meta-sra/${project}/
+rclone check ${project_dir}/*/*/01-cutadapt/assigned/* s3:ocom-edna/illumina-meta-sra/${project}/
