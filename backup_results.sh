ec74d15e6fc89004cc881f8022a32a7cd75e8505
Adam Bennett <43841526+a4000@users.noreply.github.com>
2026-07-30 10:20:58 +0800

    Update backup_results.sh

--- backup_results.sh
+++ backup_results.sh
@@ -2,8 +2,8 @@ project_dir=$1
 
 project=$(basename "$(dirname "$project_dir")")
 
-mkdir ${project_dir}/fastqs
-mv ${project_dir}/*/*/01-cutadapt/assigned/* ${project_dir}/fastqs/
-
 rclone copy ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
 rclone check ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
+
+rclone copy ${project_dir}/*/*/01-cutadapt/assigned/* s3:ocom-edna/illumina-meta-sra/${project}
+rclone check ${project_dir}/*/*/01-cutadapt/assigned/* s3:ocom-edna/illumina-meta-sra/${project}
