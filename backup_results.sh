b2bc53ebc70bc42b967958778874f02f86257eea
Adam Bennett <43841526+a4000@users.noreply.github.com>
2026-08-05 09:58:07 +0800

    Update backup_results.sh to remove project_id prefix in fastq filenames

--- backup_results.sh
+++ backup_results.sh
@@ -1,6 +1,13 @@
 project_dir=$1
 
-project=$(basename "$(dirname "$project_dir")")
+project=$(basename $(dirname $project_dir))
+
+for file in ${project_dir}/*/*/01-cutadapt/assigned/*; do
+    filename=$(basename "$file")
+    newname=${filename#${project}_}
+
+    mv $file" "$(dirname "$file")/$newname
+done
 
 rclone copy ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
 rclone check ${project_dir} s3:ocom-edna/illumina-meta-analysed-data/${project}
